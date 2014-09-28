01c290de06a0f6992105fbf5bd1a9ad3a123831b
Joey Sabey <GameFreak7744@gmail.com>
2014-09-28 17:22:28 +0100

    Refactored TJsonFields to delegate to TJsonEncode & TJsonDecode (i.e. removed all of its logic), and refactored add_json_field() to be shorthand for add_json_encode_field() & add_json_decode_field() (by taking 3 params, $name, $encode(callback) & $decode(callback), instead of 2)
    N.b. This breaks the previous public API! Until versions are tagged this type of breaking update should be highly expected!

--- TJsonFields.php
+++ TJsonFields.php
@@ -22,29 +22,17 @@
  SOFTWARE.
 */
 
+require_once('TJsonEncode.php');
+require_once('TJsonDecode.php');
+
 //Trait providing a flexible implementation of the JsonSerializable interface
 trait TJsonFields {
-    protected static $json_fields = [];
+    use TJsonEncode;
+    use TJsonDecode;
 
-    public function jsonSerialize() {
-        return array_map(function($callback) {
-            $callback = $callback->bindTo($this, $this);
-            return $callback();
-        }, static::get_json_fields());
-    }
-
-    public static function add_json_field($name, $callback) {
-        static::$json_fields[get_called_class()][$name] = $callback;
-    }
-
-    protected static function get_json_fields() {
-        $class  = get_called_class();
-        $fields = [];
-        $parent = get_parent_class($class);
-        if($parent) $fields = array_merge($fields, $parent::get_json_fields());
-        if(array_key_exists($class, static::$json_fields))
-            $fields = array_merge($fields, static::$json_fields[$class]);
-        return $fields;
+    public static function add_json_field($name, $encode, $decode) {
+        static::add_json_encode_field($name, $encode);
+        static::add_json_decode_field($name, $decode);
     }
 }
 
